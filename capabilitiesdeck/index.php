add8c8a047d584ebe3f00bcf13194915fd04cc61
Rom Gatchalian <rogmatchalian.dev@gmail.com>
2020-03-19 16:21:35 +0800

    update

--- capabilitiesdeck/index.php
+++ capabilitiesdeck/index.php
@@ -1,4 +1,4 @@
-<?
-Header( "HTTP/1.1 301 Moved Permanently" );
-Header( "Location: https://synthesisgroup.co/capabilitiesdeck/synthesis-credentials.pdf" );
+<?php
+    Header( "HTTP/1.1 301 Moved Permanently" );
+    Header( "Location: https://synthesisgroup.co/capabilitiesdeck/synthesis-credentials.pdf" );
 ?>
